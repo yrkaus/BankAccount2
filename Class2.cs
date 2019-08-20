0f7b15c2cf47333b83fe2a557eaf84a8456ba2a7
US <US.yurkaus@gmail.com>
2019-08-20 17:50:59 +0300

    add Class and BaseClass

---
+++ Class2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    public class DerivedClass : BaseClass
+    {
+
+        public void lastname()
+        {
+            base.Lastname();
+            Console.WriteLine("Введите имя Владельца счета: ");
+            Console.Read();
+        }
+        public void number()
+        {
+            base.Number();
+            Console.WriteLine("Введите номер счета: ");
+            Console.Read();
+        }
+        public void percent()
+        {
+            base.Percent();
+            Console.WriteLine("Введите процент: ");
+            Console.Read();
+        }
+        public void totalMoney()
+        {
+            base.TotalMoney();
+            Console.WriteLine("Введите баланс счета: ");
+            Console.Read();
+        }
+
+    }
+}
+
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.UTF8;
+            string sentence = "Найти сумму четных чисел и их количество в диапазоне";
+            Console.WriteLine(sentence);
+            int wordLength = 0;
+            wordLength = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                Console.WriteLine("Enter word length: ");
+                try
+                {
+                    wordLength = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Not a number");
+                }
+            }                               
+            
+            string[] words = sentence.Split(" ");
+
+            for (int i = 0; i <= words.Length - 1; i++)
+            {
+                if (words[i].Length == wordLength && wordLength >=3)
+                {
+                    words[i] = words[i].Substring(0, words[i].Length - 3) + "$$$";
+                }
+            }
+            Console.WriteLine(string.Join(" ", words));
+        }
+    }
+}
